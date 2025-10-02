922f83e7450f37ce4d9078b37cfb1f9f8b60bf02
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-10-03 00:02:04 +0600

    A online bus ticket system

--- bus_apis/models.py
+++ bus_apis/models.py
@@ -22,3 +22,16 @@ class Shedule(models.Model):
     am = models.BooleanField()
     pm = models.BooleanField()
     status = models.CharField(max_length=10, choices=Status_type.choices)
+    
+class Bus_seates(models.Model):
+    bus = models.ForeignKey(Bus_list, on_delete=models.CASCADE)
+    A1 = models.BooleanField()
+    A2 = models.BooleanField()
+    B1 = models.BooleanField()
+    B2 = models.BooleanField()
+    C1 = models.BooleanField()
+    C2 = models.BooleanField()
+    D1 = models.BooleanField()
+    D2 = models.BooleanField()
+    E1 = models.BooleanField()
+    E2 = models.BooleanField()
