e408dbd6ad3475e2706ccf63b97eeccdf9935c17
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-11-12 00:02:34 +0600

    This project is created newly for fasing the database relational problem

--- bookings/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Bookings
-
-
-class BookingsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bookings
-        fields = "__all__"
