14c2edeefad5050c466e857994dc69e07580ad42
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-11-18 01:38:22 +0600

    price addaded to shidule

--- bus/models.py
+++ bus/models.py
@@ -38,6 +38,7 @@ class Seats(models.Model):
 class Shidule(models.Model):
     route = models.ForeignKey(Route, models.CASCADE, related_name='bus_route')
     bus = models.ForeignKey(Bus, models.CASCADE, related_name='bus_shidule')
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     bus_time = models.TimeField()
     
     def __str__(self):
