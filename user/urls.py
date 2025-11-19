46f0a0804465b84811cad6f71ee6d8c481097315
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-11-20 01:00:59 +0600

    login route added

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.UsersView.as_view(), name='users'),
     path('registration/', views.UserRegistrationView.as_view(), name='user-registration'),
+    path('login/', views.LoginView, name='login'),
     path('verifi-otp/', views.VerifyOtpView, name='verifi-otp'),
     path('reset-password/', views.ResetPasswordView, name='reset-password'),
     path('forgot-password/', views.ForgotPasswordView, name='forgot-password')
